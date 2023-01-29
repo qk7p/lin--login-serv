608e713e5b055569d4780b76e8332d454a7b2e08
Zoey76 <zoey_76@msn.com>
2023-01-29 03:43:31 -0300

    Updating Dependencies
    SLF4J 1.7.32 to 2.0.6
    Java Mail API 1.6.2 to 2.1.1 (new Jakarta implementation)
    L2J Commons 2.6.5.1 to 2.6.6.1 (MariaDB Java Client 3.1.2, HikariCP
    5.0.1)
    
    Updated BitBucket pipeline.
    Update Apache Maven Wrapper.

--- src/main/java/com/l2jserver/loginserver/mail/BaseMail.java
+++ src/main/java/com/l2jserver/loginserver/mail/BaseMail.java
@@ -23,14 +23,14 @@ import static com.l2jserver.loginserver.config.Configuration.email;
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
